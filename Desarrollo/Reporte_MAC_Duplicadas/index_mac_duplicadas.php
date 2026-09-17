525ce5bba6d827659f84b7d534dcea25d52c97ce
dcarrasco007 <dcarrasco552@gmail.com>
2026-09-17 10:03:13 -0300

    ONT MAC Duplicadas

---
+++ Desarrollo/Reporte_MAC_Duplicadas/index_mac_duplicadas.php
@@ -0,0 +1,44 @@
+<?php
+//$idPag = 'vista_mac_duplicadas';
+/* $idPag2 = '999';
+include_once('../../perfiles/getPerfiles.php');
+include('../../perfiles/proceso.php');
+checkAccV2(getUser(),$idPag2); */
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SN/MAC Duplicadas</title>
+    </head>
+    <body>
+        <div class="row">
+            <div class="col-md-12 text-center"><h3>ONUs con SN/MAC en m&aacute;s de un puerto</h3></div>
+        </div><br />
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <button type="button" class="btn btn-primary" onclick="generarReporte()">Generar Datos</button>
+                &nbsp;
+                <a id="btnExportar" href="export_mac_duplicadas.php" class="btn btn-success" style="display:none;">Exportar Excel</a>
+            </div>
+        </div><br />
+        <div class="row">
+            <div class="col-md-12">
+                <div id="cont_mac_dup" class="aling-left"></div>
+            </div>
+        </div>
+    </body>
+</html>
+<script type="text/javascript">
+function generarReporte() {
+    $('#cont_mac_dup').html('<center><br>Generando informe, por favor espera...</center>');
+    $('#btnExportar').hide();
+    $.post('tabla_mac_duplicadas.php', {}, function(data) {
+        $('#cont_mac_dup').html(data);
+        $('#btnExportar').show();
+        $('#tblMacDup').tablesorter({
+            theme: 'blue',
+            widgets: ["zebra", "stickyHeaders", "filter"]
+        });
+    });
+}
+</script>
---
+++ Desarrollo/Reporte_MAC_Duplicadas/tabla_mac_duplicadas.php
@@ -0,0 +1,107 @@
+<?php
+// Reporte: ONUs con SN/MAC registrada en mas de un puerto.
+// Consulta unica con GROUP_CONCAT (evita el patron N+1: una sola query
+// para los 100+ grupos de duplicados, en vez de 1 SELECT por SN/MAC).
+include ('../../../conexion/conexion_db.php');
+$mysqli = new mysqli($host144_geret,$user144_geret,$pass144_geret, 'Aden');
+$mysqli->set_charset("utf8");
+
+$mysqli->query("SET SESSION group_concat_max_len = 1000000");
+
+$sql = "SELECT sn_mac, COUNT(*) AS veces,
+        GROUP_CONCAT(onu_name ORDER BY id SEPARATOR ' | ') AS onu_names,
+        GROUP_CONCAT(CONCAT(equipo,' F',frame_id,'/S',slot_id,'/P',port_id) ORDER BY id SEPARATOR ' | ') AS puertos,
+        GROUP_CONCAT(equipo ORDER BY id SEPARATOR ' | ') AS olts,
+        GROUP_CONCAT(estado ORDER BY id SEPARATOR ' | ') AS estados_run,
+        GROUP_CONCAT(DISTINCT line_profile_name ORDER BY line_profile_name SEPARATOR ' | ') AS profiles,
+        GROUP_CONCAT(DISTINCT modelo ORDER BY modelo SEPARATOR ' | ') AS terminales
+        FROM OLT_INFORMACION_ONT_DETALLE_COMPLETO
+        GROUP BY sn_mac
+        HAVING COUNT(*) > 1
+        ORDER BY sn_mac ASC";
+$result = $mysqli->query($sql) or die("Error consulta MAC duplicadas: " . $mysqli->error);
+
+$filas = array();
+$n_online = 0;
+$n_mixto = 0;
+$n_offline = 0;
+
+while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+    $estados = explode(' | ', $row['estados_run']);
+    $hayOnline = false;
+    $hayOffline = false;
+    foreach ($estados as $e) {
+        if ($e === 'online') { $hayOnline = true; }
+        if ($e === 'offline') { $hayOffline = true; }
+    }
+    if ($hayOnline && !$hayOffline) {
+        $clase = 'online';
+        $color = '#C6EFCE';
+        $n_online++;
+    } elseif ($hayOnline && $hayOffline) {
+        $clase = 'mixto';
+        $color = '#FFD966';
+        $n_mixto++;
+    } else {
+        $clase = 'offline';
+        $color = '#FFCCCC';
+        $n_offline++;
+    }
+
+    $estadosDisplay = str_replace(array('online', 'offline'), array('Online', 'Offline'), $row['estados_run']);
+
+    $filas[] = array(
+        'sn_mac'     => $row['sn_mac'],
+        'veces'      => $row['veces'],
+        'onu_names'  => $row['onu_names'],
+        'puertos'    => $row['puertos'],
+        'olts'       => $row['olts'],
+        'estados'    => $estadosDisplay,
+        'profiles'   => $row['profiles'],
+        'terminales' => $row['terminales'],
+        'color'      => $color,
+        'clase'      => $clase,
+    );
+}
+$mysqli->close();
+
+$total = count($filas);
+$fechaHoy = date('d/m/Y');
+
+$tabla = "<div><strong>ONUs con SN/MAC en m&aacute;s de un puerto &mdash; Access $fechaHoy</strong></div>";
+$tabla .= "<div>Total: $total SN/MAC en 2+ puertos &nbsp;|&nbsp; "
+        . "<span style='color:#2e7d32'>&#128994; Solo Online: $n_online</span>&nbsp; "
+        . "<span style='color:#f9a825'>&#128993; Mixto: $n_mixto</span>&nbsp; "
+        . "<span style='color:#c62828'>&#128308; Solo Offline: $n_offline</span></div><br>";
+
+$tabla .= "<table align='center' id='tblMacDup' class='table table-bordered table-striped' border='1' width='100%'>
+        <thead>
+        <tr>
+            <th bgcolor='#2E75B6' style='color:#FFFFFF;'><center>SN/MAC</center></th>
+            <th bgcolor='#2E75B6' style='color:#FFFFFF;'><center>Veces</center></th>
+            <th bgcolor='#2E75B6' style='color:#FFFFFF;'><center>ONU Name(s)</center></th>
+            <th bgcolor='#2E75B6' style='color:#FFFFFF;'><center>Puertos</center></th>
+            <th bgcolor='#2E75B6' style='color:#FFFFFF;'><center>OLTs</center></th>
+            <th bgcolor='#2E75B6' style='color:#FFFFFF;'><center>Estado Run</center></th>
+            <th bgcolor='#2E75B6' style='color:#FFFFFF;'><center>Line Profile(s)</center></th>
+            <th bgcolor='#2E75B6' style='color:#FFFFFF;'><center>Terminal Type</center></th>
+        </tr>
+        </thead>
+        <tbody>";
+
+foreach ($filas as $f) {
+    $tabla .= "<tr bgcolor='" . $f['color'] . "'>";
+    $tabla .= "<td>" . $f['sn_mac'] . "</td>";
+    $tabla .= "<td><center>" . $f['veces'] . "</center></td>";
+    $tabla .= "<td>" . $f['onu_names'] . "</td>";
+    $tabla .= "<td>" . $f['puertos'] . "</td>";
+    $tabla .= "<td>" . $f['olts'] . "</td>";
+    $tabla .= "<td>" . $f['estados'] . "</td>";
+    $tabla .= "<td>" . $f['profiles'] . "</td>";
+    $tabla .= "<td>" . $f['terminales'] . "</td>";
+    $tabla .= "</tr>";
+}
+$tabla .= "</tbody></table>";
+
+echo $tabla;
+?>
